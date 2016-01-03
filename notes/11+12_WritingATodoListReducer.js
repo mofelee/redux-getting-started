ea51fd7ce52422c8312aac0ee9cf89e810399d2a
Aaron Black <aaron.black@complex.com>
2016-01-03 17:16:25 -0500

    lessons 11+12, writing todolist reducers

--- notes/11+12_WritingATodoListReducer.js
+++ notes/11+12_WritingATodoListReducer.js
@@ -1,4 +1,4 @@
-// WRITING A TODOLIST REDUCER
+// WRITING A TODOLIST addTodo REDUCER
 import expect from 'expect';
 import deepFreeze from 'deep-freeze';
 
@@ -15,17 +15,6 @@ const todos = (state = [], action) => {
           completed: false
         }
       ];
-    case 'TOGGLE_TODO':
-      return state.map(todo => {
-        if (todo.id !== action.id){
-          return todo;
-        }
-
-        return {
-          ...todo,
-          completed: !todo.completed
-        };
-      })
     default:
       return state;
   }
@@ -54,44 +43,5 @@ const testAddTodo = () => {
   ).toEqual(stateAfter);
 };
 
-const testToggleTodo = () => {
-  const stateBefore = [
-    {
-      id: 0,
-      text: 'Learn Redux',
-      completed: false
-    },
-    {
-      id: 1,
-      text: 'Go shopping',
-      completed: false
-    }
-  ];
-  const action = {
-    type: 'TOGGLE_TODO',
-    id: 1
-  };
-  const stateAfter = [
-    {
-      id: 0,
-      text: 'Learn Redux',
-      completed: false
-    },
-    {
-      id: 1,
-      text: 'Go shopping',
-      completed: true
-    }
-  ];
-
-  deepFreeze(stateBefore);
-  deepFreeze(action);
-
-  expect(
-    todos(stateBefore, action)
-  ).toEqual(stateAfter);
-};
-
 testAddTodo();
-testToggleTodo();
 console.log('All tests passed.');
