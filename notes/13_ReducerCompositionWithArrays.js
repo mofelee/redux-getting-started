621505a5aa2cdd66b8e8dc694828da162fe390cf
Aaron Black <aaron.black@complex.com>
2016-01-04 22:15:16 -0500

    lessons 13 and 14

--- notes/13_ReducerCompositionWithArrays.js
+++ notes/13_ReducerCompositionWithArrays.js
@@ -1,6 +1,28 @@
-// WRITING A TODOLIST REDUCER
+// REDUCER COMPOSITION WITH ARRAYS + OBJECTS
 import expect from 'expect';
 import deepFreeze from 'deep-freeze';
+
+const todo = (state, action) => {
+  switch (action.type){
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      };
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id){
+        return state;
+      }
+
+      return {
+        ...state,
+        completed: !state.completed
+      };
+    default:
+      return state;
+  }
+};
 
 const todos = (state = [], action) => {
   switch (action.type){
@@ -9,23 +31,10 @@ const todos = (state = [], action) => {
       // and completed field to our state array
       return [
         ...state,
-        {
-          id: action.id,
-          text: action.text,
-          completed: false
-        }
+        todo(undefined, action)
       ];
     case 'TOGGLE_TODO':
-      return state.map(todo => {
-        if (todo.id !== action.id){
-          return todo;
-        }
-
-        return {
-          ...todo,
-          completed: !todo.completed
-        };
-      })
+      return state.map(t => todo(t, action));
     default:
       return state;
   }
