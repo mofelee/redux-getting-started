d2b357b3f5fb22291330b16683349dd5003acafc
Aaron Black <aaron.black@complex.com>
2016-01-17 16:39:36 -0500

    use Provider from react-redux library

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import TodoApp from './src/components/container/TodoApp';
 import { createStore } from 'redux';
 import rootReducer from './src/reducers';
-import Provider from './src/components/context';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
+  // Provider exposes props passed to it on the context
   <Provider store={createStore(rootReducer)}>
     <TodoApp />
   </Provider>,
