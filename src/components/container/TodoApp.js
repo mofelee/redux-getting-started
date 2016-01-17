720f4173def3c05cd8af3752cdf460aea983a89e
Aaron Black <aaron.black@complex.com>
2016-01-16 20:43:25 -0500

    lesson 21, extract presentational components

--- src/components/container/TodoApp.js
+++ src/components/container/TodoApp.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import FilterLink from '../FilterLink';
 import getVisibleTodos from '../../helpers/getVisibleTodos';
 import store from '../../store/store';
 
 // PRESENTATIONAL COMPONENTS
 import Todo from '../presentational/Todo';
 import TodoList from '../presentational/TodoList';
+import Footer from '../presentational/Footer';
+
+import AddTodo from '../AddTodo';
 
 let nextTodoId = 0;
+
+// NOTE diverging from Dan here because I want
+// to maintain my keydown listener
 class TodoApp extends React.Component{
-  // CONSTRUCTOR
   constructor(props){
     super(props);
-    this.addTodo = this.addTodo.bind(this);
   }
-  // LIFECYCLE METHODS
   componentDidMount(){
-    // arrow function preserves this context
-    // otherwise we'd have to do
-    // this.handleKeyDown.bind(this)
     document.addEventListener(
       'keydown',
       (e) => this.handleKeyDown(e)
@@ -30,80 +29,58 @@ class TodoApp extends React.Component{
       (e) => this.handleKeyDown(e)
     );
   }
-  // METHODS
+  addTodo(){
+    const todoInput = document.getElementById('todoInput');
+    store.dispatch({
+      type: 'ADD_TODO',
+      id: nextTodoId++,
+      text: todoInput.value
+    });
+    todoInput.value = '';
+  }
   handleKeyDown(e){
     const ENTER = 13;
     if (e.keyCode === ENTER){
       this.addTodo();
     }
   }
-  addTodo(){
-    const todoInput = document.getElementById('todoInput');
-    store.dispatch({
-      type: 'ADD_TODO',
-      text: todoInput.value,
-      id: nextTodoId++
-    });
-    todoInput.value = '';
-  }
-  // RENDER
   render(){
-
-    const {
-      todos,
-      addTodo,
-      visibilityFilter
-    } = this.props;
-
-    const visibleTodos = getVisibleTodos(
-      todos,
-      visibilityFilter
-    );
-
+    const { todos, visibilityFilter } = this.props;
     return(
       <div>
-        <input id='todoInput' ref={node => {
-          this.input = node;
-        }} />
-        <button onClick={this.addTodo}>
-          Add Todo
-        </button>
-        <TodoList
-          todos={visibleTodos}
-          onTodoClick={id =>
-            store.dispatch({
-              type: 'TOGGLE_TODO',
-              id
-            })
-          }
-        />
-        <p>
-          Show:
-          {' '}
-          <FilterLink
-           filter='SHOW_ALL'
-           currentFilter={visibilityFilter}
-          >
-            All
-          </FilterLink>
-          {' '}
-          <FilterLink
-           filter='SHOW_ACTIVE'
-           currentFilter={visibilityFilter}
-          >
-            Active
-          </FilterLink>
-          {' '}
-          <FilterLink
-           filter='SHOW_COMPLETED'
-           currentFilter={visibilityFilter}
-          >
-            Completed
-          </FilterLink>
-        </p>
+      <AddTodo
+        onAddClick={text =>
+          store.dispatch({
+            type: 'ADD_TODO',
+            id: nextTodoId++,
+            text
+          })
+        }
+      />
+      <TodoList
+        todos={getVisibleTodos(
+          todos,
+          visibilityFilter
+        )}
+        onTodoClick={id =>
+          store.dispatch({
+            type: 'TOGGLE_TODO',
+            id
+          })
+        }
+      />
+      <Footer
+        visibilityFilter={visibilityFilter}
+        onFilterClick={filter =>
+          store.dispatch({
+            type: 'SET_VISIBILITY_FILTER',
+            filter
+          })
+        }
+      />
       </div>
     );
   }
-}
+};
 
 export default TodoApp;
--- src/components/presentational/FilterLink.js
+++ src/components/presentational/FilterLink.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import store from '../store/store';
+import store from '../../store/store';
 
 const FilterLink = ({
   filter,
   currentFilter,
-  children
+  children,
+  onClick
 }) => {
   if (filter === currentFilter){
     return <span>{children}</span>
@@ -14,10 +15,7 @@ const FilterLink = ({
     <a href='#'
       onClick={e => {
         e.preventDefault();
-        store.dispatch({
-          type: 'SET_VISIBILITY_FILTER',
-          filter
-        });
+        onClick(filter);
       }}
     >
       {children}
